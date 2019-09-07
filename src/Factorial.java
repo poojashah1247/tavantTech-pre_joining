80b49014464215f66e76c1441ecfd3f972ba7352
poojashah1247 <52858032+poojashah1247@users.noreply.github.com>
2019-09-07 11:44:46 +0530

    Update Factorial.java

--- src/Factorial.java
+++ src/Factorial.java
@@ -3,8 +3,8 @@ public class Factorial {
 	public static void main(String args[]) 
 	{
 		int n=5;
-		Factorial f = new Factorial();
-		int facto = f.factorial(n);
+		Factorial fact = new Factorial();
+		int facto = fact.factorial(n);
 		System.out.println("factorial of 5 is : "+facto);
 	}
 	static int factorial(int n) 
